74bcc5dc8f225c656a7c775ab709a6819beab1b0
Jefferson <jeffersonkr@hotmail.com>
2019-06-04 17:18:00 -0300

    remocao de models nao utilizados

--- core/models/Boleto.py
+++ core/models/Boleto.py
@@ -1,22 +1,9 @@
 from django.db import models
-from .Aluguel import Aluguel
-from .Venda import Venda
-from .Cliente import Cliente
-from .Proprietario import Proprietario
-
+from core.models.Contrato import Contrato
 
 
 class Boleto(models.Model):
-    #id_boleto = models.IntegerField(primary_key=True)
-    id_aluguel = models.ForeignKey(Aluguel, models.DO_NOTHING, db_column='id_aluguel', blank=True, null=True)
-    id_venda = models.ForeignKey(Venda, models.DO_NOTHING, db_column='id_venda', blank=True, null=True)
-    id_cliente = models.ForeignKey(Cliente, models.DO_NOTHING, db_column='id_cliente', blank=True, null=True)
-    id_proprietario = models.ForeignKey(Proprietario, models.DO_NOTHING, db_column='id_proprietario', blank=True, null=True)
-    data_emissao = models.DateField(blank=True, null=True)
-    data_vencimento = models.DateField(blank=True, null=True)
-    valor_boleto = models.FloatField(blank=True, null=True)
-    multa_juros = models.FloatField(blank=True, null=True)
-    status = models.CharField(max_length=100, blank=True, null=True)
+    contrato = models.ForeignKey(Contrato, on_delete=models.DO_NOTHING)
 
     class Meta:
         managed = True
