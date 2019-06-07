e76422651fdf13fe50498d37479458f1f58db21e
jeffersonkr <jeffersonkr@hotmail.com>
2019-06-06 23:40:49 -0300

    add static root

--- SistemaImobiliaria/settings.py
+++ SistemaImobiliaria/settings.py
@@ -144,8 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # auth
 LOGIN_URL = '/login/'
